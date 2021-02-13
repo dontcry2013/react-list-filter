c75378fbb71977fc3c994414e93f176ee423e1ee
Zac <zach.qin@gmail.com>
2021-02-13 21:32:46 +1100

    Updates

---
+++ precache-manifest.0cfb5c62b0b20967529eda9b3b85caba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f4543b3885aa33641509b9efd3aa12",
+    "url": "./index.html"
+  },
+  {
+    "revision": "77789aedab8b4d15e5f6",
+    "url": "./static/css/2.f5ac1ad6.chunk.css"
+  },
+  {
+    "revision": "77789aedab8b4d15e5f6",
+    "url": "./static/js/2.ed8f2c04.chunk.js"
+  },
+  {
+    "revision": "53dc326c48413353a978",
+    "url": "./static/js/main.695f42b0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
